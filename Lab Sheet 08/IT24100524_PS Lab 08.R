bb4111bcfcd0449d7640dee2ab49925f74902fdf
IT24100524 <IT24100524@my.sliit.lk>
2025-09-26 13:57:04 +0530

    IT24100524_PS Lab 08

---
+++ Lab Sheet 08/IT24100524_PS Lab 08.R
@@ -0,0 +1,47 @@
+setwd("C:\\Users\\Asus\\Desktop\\IT24100524\\PS Lab 08")
+getwd()
+
+data <- read.table("Exercise - LaptopsWeights.txt", header = TRUE)
+
+fix(data)
+
+attach(data)
+
+weights <- data$Weight.kg.
+
+weights
+
+pop_mean <- mean(weights)
+
+pop_var <- mean((weights - pop_mean)^2)
+
+pop_sd <- sqrt(pop_var)
+
+pop_mean
+pop_var
+pop_sd
+
+
+samples <- c()
+n <- c()
+
+for (i in 1:25) {
+  s <- sample(weights, 6, replace = TRUE)
+  samples <- cbind(samples, s)
+  n <- c(n, paste('S', i))
+}
+
+
+colnames(samples) <- n
+
+
+s.means <- apply(samples, 2, mean)
+s.means
+s.vars  <- apply(samples, 2, var)
+s.vars
+
+samplemean <- mean(s.means)
+samplemean
+sampleSD   <- sd(s.means)
+sampleSD
+
